59299d9461883fa7d1c4a9a9dd12dd08268ea72f
Paul Birsan <paul@distancify.com>
2019-09-02 12:01:32 +0300

    Update the range picker with the required functionality

--- src/components/widgets/activator.vue
+++ src/components/widgets/activator.vue
@@ -1,27 +1,104 @@
 <template>
-  <div class="activator-wrapper">
-    <input v-bind="$attrs" class="text-field" :class="inputClass" type="text" :value="value">
-    <span v-if="picker.showActivatorBar" class="bar"></span>
-    <label v-if="picker.showActivatorLabel">Select Date</label>
-  </div>
+    <div class="activator-wrapper">
+        <input v-bind="$attrs" class="text-field dateRangeActivatorInput" :class="inputClass" type="text" :value="value">
+        <span v-if="picker.showActivatorBar" class="bar"></span>
+        <label v-if="picker.showActivatorLabel">Select Date</label>
+        <div class="activatorSelectBox" @click="onSelectBoxClicked($event)"></div>
+    </div>
 </template>
 
 <script>
-export default {
-  inheritAttrs: false,
-  inject: ['picker'],
-  props: ['value'],
-  computed: {
-    inputClass () {
-      return {
-        'text-field__filled': this.value.trim().length > 0,
-      };
-    },
-  },
-};
+    export default {
+        inheritAttrs: false,
+        inject: ['picker'],
+        props: ['value', 'isPickerActive'],
+        data() {
+            return {
+                selectBoxElement: "",
+                datePickerPanel: ""
+            }
+        },
+        watch: {
+            isPickerActive: function (isPickerActive) {
+                if (isPickerActive) {
+                    this.selectBoxElement.classList.add("active");
+                }
+                else {
+                    this.selectBoxElement.classList.remove("active");
+                }
+            }
+        },
+        computed: {
+            inputClass() {
+                return {
+                    'text-field__filled': this.value.trim().length > 0,
+                };
+            },
+        },
+        methods: {
+            onSelectBoxClicked(event) {
+                if (this.isPickerActive) {
+                    event.stopPropagation();
+                    this.$parent.closeRangePicker();
+                }
+            }
+        },
+        mounted() {
+            this.selectBoxElement = document.querySelector(".activatorSelectBox");
+        }
+    };
 </script>
 
 
 <style lang="scss">
-@import '../../styles/components/activator.scss';
+    @import '../../styles/components/activator.scss';
+</style>
+
+<style>
+
+    .activator-wrapper {
+        min-height: 38px;
+        display: flex;
+        flex-grow: 1;
+    }
+
+        .activator-wrapper .text-field.dateRangeActivatorInput {
+            display: block;
+            padding: 9px 16px;
+            width: 100%;
+            border: 1px solid #C2C8CC;
+            box-shadow: none;
+            background-color: white;
+            color: #2f3941;
+            outline: none;
+            font-size: 13px;
+            letter-spacing: 0.2px;
+        }
+
+    .activatorSelectBox {
+        position: absolute;
+        width: 26px;
+        height: 26px;
+        right: 7px;
+        top: 5px;
+        text-align: center;
+        transition: transform .2s ease;
+        cursor: pointer;
+    }
+
+        .activatorSelectBox.active {
+            transform: rotate(180deg);
+        }
+
+        .activatorSelectBox:before {
+            position: relative;
+            right: 0;
+            top: 65%;
+            color: #999;
+            margin-top: 4px;
+            border-color: #999 transparent transparent;
+            border-style: solid;
+            border-width: 5px 5px 0;
+            content: "";
+        }
 </style>
--- src/constant/index.js
+++ src/constant/index.js
@@ -6,36 +6,52 @@ import moment from 'moment';
  * @param { Number } endOffset end date 偏移
  * @param { Number } period 时间维度：day or month
  */
-function getRange (startOffset = 0, endOffset = 0, period = 'day') {
-  return [
-    moment().subtract(startOffset, period).startOf(period),
-    moment().subtract(endOffset, period).endOf(period),
-  ];
+function getRange(startOffset = 0, endOffset = 0, period = 'day') {
+    return [
+        moment().subtract(startOffset, period).startOf(period),
+        moment().subtract(endOffset, period).endOf(period)
+    ];
+}
+
+function getDayOfWeek() {
+    var day = moment().isoWeekday();
+    return day;
 }
 
 export const defaultPresets = [
-  {
-    label: 'Today',
-    range: getRange(0, 0),
-  },
-  {
-    label: 'Yesterday',
-    range: getRange(1, 1),
-  },
-  {
-    label: 'Last 7 Days',
-    range: getRange(6, 0),
-  },
-  {
-    label: 'Last 30 Days',
-    range: getRange(29, 0),
-  },
-  {
-    label: 'This Month',
-    range: getRange(0, 0, 'month'),
-  },
-  {
-    label: 'Last Month',
-    range: getRange(1, 1, 'month'),
-  },
+    {
+        label: 'Today',
+        range: getRange(0, 0)
+    },
+    {
+        label: 'Yesterday',
+        range: getRange(1, 1)
+    },
+    {
+        label: 'This week',
+        range: getRange(getDayOfWeek() - 1, 0)
+    },
+    {
+        label: 'Last week',
+        range: getRange(6 + getDayOfWeek(), getDayOfWeek())
+    },
+    {
+        label: 'This Month',
+        range: getRange(0, 0, 'month')
+    },
+    {
+        label: 'Last Month',
+        range: getRange(1, 1, 'month')
+    },
+    {
+        label: 'This Quarter',
+        range: [
+            moment().quarter(moment().quarter()).startOf('quarter'),
+            moment()
+        ]
+    },
+    {
+        label: 'Last Quarter',
+        range: getRange(1, 1, 'quarter')
+    }
 ];
